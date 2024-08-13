aca948b8c07cfebe706a38e0c259e12fde1e833c
Fabiano Brito <fabiano99.fbs@gmail.com>
2024-08-12 21:47:36 -0300

    v.1

--- dados.sql
+++ dados.sql
@@ -1,6 +1,20 @@
 CREATE DATABASE bibliota_web;
 use bibliota_web;
 
+CREATE TABLE usuarios(
+id_user INT PRIMARY KEY AUTO_INCREMENT,
+nome_login VARCHAR(100) UNIQUE,
+email VARCHAR(255) UNIQUE,
+senha VARCHAR(50)
+);
+
+INSERT INTO usuarios(nome_login, email, senha)
+VALUES('fabiano', NULL, 12345678),
+(NULL, 'fabiano@gmail.com', 12345678);
+
+SELECT *FROM usuarios;
+
+
 CREATE TABLE livros(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome_livro VARCHAR(255) NOT NULL,
@@ -13,8 +27,8 @@ status1 VARCHAR(50)
 
 INSERT INTO livros (nome_livro, autor, foto_capa, categoria, descricao, status1) 
 VALUES ('O Peregrino', 'John Bunyan', './capas-livro/O-peregrino-2.jpg', 'devocional', 'DEPOIS DA BÍBLIA, ESTE É O LIVRO MAIS LIDO EM TODO O MUNDO HÁ MAIS DE TRÊS SÉCULOS. O PEREGRINO, DO INGLÊS JOHN BUNYAN, MANTÉM-SE ATUAL PORQUE TRATA DA ESSÊNCIA DA VIDA HUMANA: A BUSCA DA FELICIDADE Como o leitor perceberá nesta história alegórica, o que o personagem Cristão enfrenta em sua caminhada da Cidade da Destruição para a Cidade Celestial não é diferente do que sempre aconteceu e ainda está presente na trajetória da humanidade: perigos, maldades, traições, mas também boa vontade, sabedoria e esperança. Traduzido em mais de 200 idiomas, O Peregrino cativa ainda pela beleza e simplicidade do texto. Nesta versão atualizada, Claudio Blanc usa a linguagem de hoje, sem deixar escapar nada da riqueza do original. Ele recria nomes de personagens, bons e maus, bem fiéis ao que se vê hoje no Brasil e pelo mundo afora, como Amor-ao-Dinheiro, Bajulação, Caridade, Duas Caras, Glória Mundana, Hipocrisia, Ignorância, Legalidade, Ócio, Piedade, Pretensioso e Vira-Casaca.', 'disponivel'),
-('Mere Christianity', 'C.S. Lewis', './capas-livro/mere.jpg', 'devocional', ' a', 'disponivel'),
-('O Evangelho Maltrapilho', 'Brennan Manning', './capas-livro/O-evangelho-maltrapilho.png', 'devocional', '"O evangelho maltrapilho" foi escrito para pessoas aniquiladas, derrotadas e exauridas. Pessoas que se acham indignas de receber o amor de Deus. Quem sabe, ignoradas pela comunidade de cristãos por não se encaixarem no perfil de super-homem ou de supermulher que lhes é constantemente exigido. Pessoas cansadas da espiritualidade superficial e consumista. Pessoas que travam inúmeras batalhas interiores por não se sentirem parte de uma comunidade afetiva e acolhedora. É um livro que escrevi para mim mesmo e para quem quer que tenha ficado cansado e desencorajado ao longo do Caminho, confessa o autor. Franco e provocador, o aclamado filósofo e teólogo cristão Brennan Manning estréia em língua portuguesa com sua principal obra, que nos convida a depositar nossa esperança na amplitude da graça, capaz de alcançar pecadores e pobres em espírito, e de resgatar nossa dignidade original. No mínimo, você não ficará indiferente a ela.', 'alugado'),
+('Mere Christianity', 'C.S. Lewis', './capas-livro/mere.jpg', 'devocional', ' a', 'reservado'),
+('O Evangelho Maltrapilho', 'Brennan Manning', './capas-livro/O-evangelho-maltrapilho.png', 'devocional', '"O evangelho maltrapilho" foi escrito para pessoas aniquiladas, derrotadas e exauridas. Pessoas que se acham indignas de receber o amor de Deus. Quem sabe, ignoradas pela comunidade de cristãos por não se encaixarem no perfil de super-homem ou de supermulher que lhes é constantemente exigido. Pessoas cansadas da espiritualidade superficial e consumista. Pessoas que travam inúmeras batalhas interiores por não se sentirem parte de uma comunidade afetiva e acolhedora. É um livro que escrevi para mim mesmo e para quem quer que tenha ficado cansado e desencorajado ao longo do Caminho, confessa o autor. Franco e provocador, o aclamado filósofo e teólogo cristão Brennan Manning estréia em língua portuguesa com sua principal obra, que nos convida a depositar nossa esperança na amplitude da graça, capaz de alcançar pecadores e pobres em espírito, e de resgatar nossa dignidade original. No mínimo, você não ficará indiferente a ela.', 'reservado'),
 ('A Cabana', 'William P. Young', './capas-livro/a-cabana.webp', 'devocional', ' a', 'disponivel'),
 ('Em Busca de Deus', 'A.W. Tozer', './capas-livro/em-busca-de-deus.jpg', 'devocional', ' a', 'disponivel'),
 ('O Poder da Mulher que Ora', 'Stormie Omartian', './capas-livro/o-poder-da-mulher.jpg', 'devocional', ' a', 'disponivel'),
